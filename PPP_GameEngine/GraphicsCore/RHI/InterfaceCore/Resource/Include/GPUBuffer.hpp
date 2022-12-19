40c2366f015b880388f7f2bde715805e53fcc1c3
YutaroToide <toide213@gmail.com>
2022-12-19 23:31:27 +0900

    リファクタリング

--- PPP_GameEngine/GraphicsCore/RHI/InterfaceCore/Resource/Include/GPUBuffer.hpp
+++ PPP_GameEngine/GraphicsCore/RHI/InterfaceCore/Resource/Include/GPUBuffer.hpp
@@ -39,34 +39,47 @@ namespace rhi::core
 		//          Create temp upload buffer and copy this to default buffer (default buffer uses commandList)
 		//           https://zenn.dev/lriki/scraps/5bb7f5a23bba16 (ç°å„ämîFÇµÇΩÇ¢)
 		virtual void Pack(const void* data, const std::shared_ptr<RHICommandList>& commandList = nullptr) = 0;
+		
 		// @brief : Call at once in each frame (If you need). CopyStart + CopyTotalData + CopyEnd. 
 		void         Update(const void* data, const size_t dataLength);
+		
 		// @brief : Begin Map Function
 		virtual void CopyStart() = 0;
+		
 		// @brief : GPU copy to one element 
 		virtual void CopyData(const void* data, const size_t elementIndex) = 0;
+		
 		/* @brief : GPU copy the specified range*/
 		virtual void CopyTotalData(const void* data, const size_t dataLength, const size_t indexOffset = 0) = 0;
+		
 		// @brief : Unmap Function
 		virtual void CopyEnd() = 0;
+
 		/****************************************************************************
 		**                Public Member Variables
 		*****************************************************************************/
 		// @brief : Return Buffer Array Length
 		size_t GetElementCount   () const { return _metaData.Count; }
+
 		// @brief : Return Buffer Element Byte Size
 		size_t GetElementByteSize() const { return _metaData.Stride; }
+
 		// @brief : Return Count * Stride
 		size_t GetTotalByteSize  () const { return _metaData.ByteSize; }
+
 		// @brief : Return GPU Resource Type. (Basically Buffer or RaytracingAccelerationStructure) 
 		ResourceType  GetResourceType() const { return _metaData.ResourceType; }
+		
 		// @brief : Return GPU Resource State
 		ResourceState GetResourceState() const { return _metaData.State; }
+		
 		// @brief : Return Buffer Usage Flag. (Vertex, Index, or Constant Buffer)
 		ResourceUsage GetUsage() const { return _metaData.ResourceUsage; }
+		
 		// @brief : Return Buffer Type
 		BufferType GetBufferType() const { return _metaData.BufferType; }
-		      GPUBufferMetaData& GetMetaData()                { return _metaData; }
+		
+		GPUBufferMetaData& GetMetaData()                      { return _metaData; }
 		const GPUBufferMetaData& GetMetaData() const noexcept { return _metaData; }
 		
 		/****************************************************************************
@@ -78,8 +91,11 @@ namespace rhi::core
 		**                Constructor and Destructor
 		*****************************************************************************/
 		GPUBuffer() = default;
+
 		~GPUBuffer() = default;
+
 		explicit GPUBuffer(const std::shared_ptr<RHIDevice>& device, const core::GPUBufferMetaData& metaData);
+		
 		/****************************************************************************
 		**                Protected Function
 		*****************************************************************************/
