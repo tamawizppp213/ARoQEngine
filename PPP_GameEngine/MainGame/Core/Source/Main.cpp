c4474c47c97b74ce0a4d998fff0b3f8f35fdf59e
YutaroToide <toide213@gmail.com>
2023-05-02 23:52:54 +0900

    test

--- PPP_GameEngine/MainGame/Core/Source/Main.cpp
+++ PPP_GameEngine/MainGame/Core/Source/Main.cpp
@@ -9,7 +9,7 @@ int main()
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCommandLine, _In_ int nShowCommand)
 #endif
 {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+   ]][]
     
     /********************************************
     **         Initialize
