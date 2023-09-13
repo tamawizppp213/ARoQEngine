4e6497f9cddad54c0f8566ce0edb0b4bfe5cb1fd
Toide Yutaro <toide213@gmail.com>
2023-09-14 08:05:49 +0900

    add error対応

---
+++ PPP_GameEngine/Platform/Windows/Source/WIndowsError.cpp
@@ -0,0 +1,39 @@
+//////////////////////////////////////////////////////////////////////////////////
+//              @file   WindowsError.cpp
+///             @brief  Log windows error code and error sentence for debugging
+///             @author Toide Yutaro
+///             @date   2023_09_13
+//////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////
+//                             Include
+//////////////////////////////////////////////////////////////////////////////////
+#ifdef _WIN32
+#include "../../Windows/Include/WindowsError.hpp"
+#include <stdexcept>
+//////////////////////////////////////////////////////////////////////////////////
+//                              Define
+//////////////////////////////////////////////////////////////////////////////////
+using namespace platform;
+using namespace platform::windows;
+
+//////////////////////////////////////////////////////////////////////////////////
+//                              Implement
+//////////////////////////////////////////////////////////////////////////////////
+void ErrorLogger::Log(HWND hwnd)
+{
+	const std::uint32_t errorCode = ::GetLastError();
+
+	LPVOID messageBuffer = NULL;
+	FormatMessage
+	(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | // テキストのメモリ割り当てを要求する
+		FORMAT_MESSAGE_FROM_SYSTEM | // エラーメッセージはWindowsが用意している
+		FORMAT_MESSAGE_IGNORE_INSERTS,   // 次の引数を無視してエラーコードに対するエラーメッセージを作成する
+		NULL, errorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&messageBuffer, 0, NULL
+	);
+
+	MessageBox(hwnd, (LPCTSTR)messageBuffer, NULL, MB_OK | MB_ICONINFORMATION);
+	LocalFree(messageBuffer);
+}
+#endif
