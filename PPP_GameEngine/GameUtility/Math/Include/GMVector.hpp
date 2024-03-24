8842e567c50730604a4845cd0c31f7403c26cb70
Toide Yutaro <toide213@gmail.com>
2024-03-25 01:29:02 +0900

    fix　

--- PPP_GameEngine/GameUtility/Math/Include/GMVector.hpp
+++ PPP_GameEngine/GameUtility/Math/Include/GMVector.hpp
@@ -1,23 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 ///             @file   GMVector.hpp
 ///             @brief  GMVector
-///             @author James Stanard  // Partially modified by Toide.
-///             @date   2021_05_09 (modified)
+///             @author Toide
+///             @date   2024_03_25
 //////////////////////////////////////////////////////////////////////////////////
-
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-// Developed by Minigraph
-//
-// Author:  James Stanard 
-//
-
 #pragma once
 #ifndef GM_VECTOR_HPP
 #define GM_VECTOR_HPP
