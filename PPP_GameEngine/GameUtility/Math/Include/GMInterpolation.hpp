e36b5c3ec907aea466d31098d483a2141d8a1516
Toide Yutaro <toide213@gmail.com>
2024-03-24 22:57:06 +0900

    fix エラー修正

--- PPP_GameEngine/GameUtility/Math/Include/GMInterpolation.hpp
+++ PPP_GameEngine/GameUtility/Math/Include/GMInterpolation.hpp
@@ -28,14 +28,14 @@ namespace gm
 		/****************************************************************************
 		**                Public Function
 		*****************************************************************************/
-		template<typename T>INLINE static T        CubicInterpolate(T const& startPoint, T const& controlPoint1, T const& controlPoint2, T const& endPoint, float t);
-		template<typename T>INLINE static T        CubicTangent    (T const& startPoint, T const& controlPoint1, T const& controlPoint2, T const& endPoint, float t);
-		template<         > INLINE static Vector2  CubicInterpolate(Vector2 const& startPoint, Vector2 const& controlPoint1, Vector2 const& controlPoint2, Vector2 const& endPoint, float t);
-		template<         > INLINE static Vector2  CubicTangent    (Vector2 const& startPoint, Vector2 const& controlPoint1, Vector2 const& controlPoint2, Vector2 const& endPoint, float t);
-		template<         > INLINE static Vector3  CubicInterpolate(Vector3 const& startPoint, Vector3 const& controlPoint1, Vector3 const& controlPoint2, Vector3 const& endPoint, float t);
-		template<         > INLINE static Vector3  CubicTangent    (Vector3 const& startPoint, Vector3 const& controlPoint1, Vector3 const& controlPoint2, Vector3 const& endPoint, float t);
-		template<         > INLINE static Vector4  CubicInterpolate(Vector4 const& startPoint, Vector4 const& controlPoint1, Vector4 const& controlPoint2, Vector4 const& endPoint, float t);
-		template<         > INLINE static Vector4  CubicTangent    (Vector4 const& startPoint, Vector4 const& controlPoint1, Vector4 const& controlPoint2, Vector4 const& endPoint, float t);
+		template<typename T>__forceinline static T        CubicInterpolate(T const& startPoint, T const& controlPoint1, T const& controlPoint2, T const& endPoint, float t);
+		template<typename T>__forceinline static T        CubicTangent    (T const& startPoint, T const& controlPoint1, T const& controlPoint2, T const& endPoint, float t);
+		template<         > __forceinline static Vector2f  CubicInterpolate(Vector2f const& startPoint, Vector2f const& controlPoint1, Vector2f const& controlPoint2, Vector2f const& endPoint, float t);
+		template<         > __forceinline static Vector2f  CubicTangent    (Vector2f const& startPoint, Vector2f const& controlPoint1, Vector2f const& controlPoint2, Vector2f const& endPoint, float t);
+		template<         > __forceinline static Vector3f  CubicInterpolate(Vector3f const& startPoint, Vector3f const& controlPoint1, Vector3f const& controlPoint2, Vector3f const& endPoint, float t);
+		template<         > __forceinline static Vector3f  CubicTangent    (Vector3f const& startPoint, Vector3f const& controlPoint1, Vector3f const& controlPoint2, Vector3f const& endPoint, float t);
+		template<         > __forceinline static Vector4f  CubicInterpolate(Vector4f const& startPoint, Vector4f const& controlPoint1, Vector4f const& controlPoint2, Vector4f const& endPoint, float t);
+		template<         > __forceinline static Vector4f  CubicTangent    (Vector4f const& startPoint, Vector4f const& controlPoint1, Vector4f const& controlPoint2, Vector4f const& endPoint, float t);
 		/****************************************************************************
 		**                Public Member Variables
 		*****************************************************************************/
@@ -61,14 +61,14 @@ namespace gm
 				endPoint * t * t * t;
 	}
 
-	template<>Vector2 Bezier::CubicInterpolate(Vector2 const& startPoint, Vector2 const& controlPoint1, Vector2 const& controlPoint2, Vector2 const& endPoint, float t)
+	template<>Vector2f Bezier::CubicInterpolate(Vector2f const& startPoint, Vector2f const& controlPoint1, Vector2f const& controlPoint2, Vector2f const& endPoint, float t)
 	{
-		Vector2 T0 = ReplicateVector2((1 - t) * (1 - t) * (1 - t));
-		Vector2 T1 = ReplicateVector2(3 * t * (1 - t) * (1 - t));
-		Vector2 T2 = ReplicateVector2(3 * t * t * (1 - t));
-		Vector2 T3 = ReplicateVector2(t * t * t);
+		Vector2f T0 = Vector2f((1 - t) * (1 - t) * (1 - t));
+		Vector2f T1 = Vector2f(3 * t * (1 - t) * (1 - t));
+		Vector2f T2 = Vector2f(3 * t * t * (1 - t));
+		Vector2f T3 = Vector2f(t * t * t);
 
-		Vector2 Result = startPoint * T0;
+		Vector2f Result = startPoint * T0;
 		Result = controlPoint1 * T1 + Result;
 		Result = controlPoint2 * T2 + Result;
 		Result = endPoint * T3 + Result;
@@ -76,14 +76,14 @@ namespace gm
 		return Result;
 	}
 
-	template<>Vector3 Bezier::CubicInterpolate(Vector3 const& startPoint, Vector3 const& controlPoint1, Vector3 const& controlPoint2, Vector3 const& endPoint, float t)
+	template<>Vector3f Bezier::CubicInterpolate(Vector3f const& startPoint, Vector3f const& controlPoint1, Vector3f const& controlPoint2, Vector3f const& endPoint, float t)
 	{
-		Vector3 T0 = ReplicateVector3((1 - t) * (1 - t) * (1 - t));
-		Vector3 T1 = ReplicateVector3(3 * t * (1 - t) * (1 - t));
-		Vector3 T2 = ReplicateVector3(3 * t * t * (1 - t));
-		Vector3 T3 = ReplicateVector3(t * t * t);
+		Vector3f T0 = Vector3f((1 - t) * (1 - t) * (1 - t));
+		Vector3f T1 = Vector3f(3 * t * (1 - t) * (1 - t));
+		Vector3f T2 = Vector3f(3 * t * t * (1 - t));
+		Vector3f T3 = Vector3f(t * t * t);
 
-		Vector3 Result = startPoint * T0;
+		Vector3f Result = startPoint * T0;
 		Result = controlPoint1 * T1 + Result;
 		Result = controlPoint2 * T2 + Result;
 		Result = endPoint * T3 + Result;
@@ -91,14 +91,14 @@ namespace gm
 		return Result;
 	}
 
-	template<>Vector4 Bezier::CubicInterpolate(Vector4 const& startPoint, Vector4 const& controlPoint1, Vector4 const& controlPoint2, Vector4 const& endPoint, float t)
+	template<>Vector4f Bezier::CubicInterpolate(Vector4f const& startPoint, Vector4f const& controlPoint1, Vector4f const& controlPoint2, Vector4f const& endPoint, float t)
 	{
-		Vector4 T0 = ReplicateVector4((1 - t) * (1 - t) * (1 - t));
-		Vector4 T1 = ReplicateVector4(3 * t * (1 - t) * (1 - t));
-		Vector4 T2 = ReplicateVector4(3 * t * t * (1 - t));
-		Vector4 T3 = ReplicateVector4(t * t * t);
+		Vector4f T0 = Vector4f((1 - t) * (1 - t) * (1 - t));
+		Vector4f T1 = Vector4f(3 * t * (1 - t) * (1 - t));
+		Vector4f T2 = Vector4f(3 * t * t * (1 - t));
+		Vector4f T3 = Vector4f(t * t * t);
 
-		Vector4 Result = startPoint * T0;
+		Vector4f Result = startPoint * T0;
 		Result = controlPoint1 * T1 + Result;
 		Result = controlPoint2 * T2 + Result;
 		Result = endPoint * T3 + Result;
@@ -114,14 +114,14 @@ namespace gm
 			endPoint      * (t * t);
 	}
 
-	template<> Vector2 Bezier::CubicTangent(Vector2 const& startPoint, Vector2 const& controlPoint1, Vector2 const& controlPoint2, Vector2 const& endPoint, float t) noexcept
+	template<> Vector2f Bezier::CubicTangent(Vector2f const& startPoint, Vector2f const& controlPoint1, Vector2f const& controlPoint2, Vector2f const& endPoint, float t) noexcept
 	{
-		Vector2 T0 = ReplicateVector2(-1 + 2 * t - t * t);
-		Vector2 T1 = ReplicateVector2(1 - 4 * t + 3 * t * t);
-		Vector2 T2 = ReplicateVector2(2 * t - 3 * t * t);
-		Vector2 T3 = ReplicateVector2(t * t);
+		Vector2f T0 = Vector2f(-1 + 2 * t - t * t);
+		Vector2f T1 = Vector2f(1 - 4 * t + 3 * t * t);
+		Vector2f T2 = Vector2f(2 * t - 3 * t * t);
+		Vector2f T3 = Vector2f(t * t);
 
-		Vector2 Result = startPoint * T0;
+		Vector2f Result = startPoint * T0;
 		Result = controlPoint1 * T1 + Result;
 		Result = controlPoint2 * T2 + Result;
 		Result = endPoint * T3 + Result;
@@ -129,28 +129,28 @@ namespace gm
 		return Result;
 	}
 
-	template<> Vector3 Bezier::CubicTangent(Vector3 const& startPoint, Vector3 const& controlPoint1, Vector3 const& controlPoint2, Vector3 const& endPoint, float t) noexcept
+	template<> Vector3f Bezier::CubicTangent(Vector3f const& startPoint, Vector3f const& controlPoint1, Vector3f const& controlPoint2, Vector3f const& endPoint, float t) noexcept
 	{
-		Vector3 T0 = ReplicateVector3(-1 + 2 * t - t * t);
-		Vector3 T1 = ReplicateVector3(1 - 4 * t + 3 * t * t);
-		Vector3 T2 = ReplicateVector3(2 * t - 3 * t * t);
-		Vector3 T3 = ReplicateVector3(t * t);
+		Vector3f T0 = Vector3f(-1 + 2 * t - t * t);
+		Vector3f T1 = Vector3f(1 - 4 * t + 3 * t * t);
+		Vector3f T2 = Vector3f(2 * t - 3 * t * t);
+		Vector3f T3 = Vector3f(t * t);
 
-		Vector3 Result = startPoint * T0;
+		Vector3f Result = startPoint * T0;
 		Result = controlPoint1 * T1 + Result;
 		Result = controlPoint2 * T2 + Result;
 		Result = endPoint * T3 + Result;
 
 		return Result;
 	}
-	template<> Vector4 Bezier::CubicTangent(Vector4 const& startPoint, Vector4 const& controlPoint1, Vector4 const& controlPoint2, Vector4 const& endPoint, float t) noexcept
+	template<> Vector4f Bezier::CubicTangent(Vector4f const& startPoint, Vector4f const& controlPoint1, Vector4f const& controlPoint2, Vector4f const& endPoint, float t) noexcept
 	{
-		Vector4 T0 = ReplicateVector4(-1 + 2 * t - t * t);
-		Vector4 T1 = ReplicateVector4(1 - 4 * t + 3 * t * t);
-		Vector4 T2 = ReplicateVector4(2 * t - 3 * t * t);
-		Vector4 T3 = ReplicateVector4(t * t);
+		Vector4f T0 = Vector4f(-1 + 2 * t - t * t);
+		Vector4f T1 = Vector4f(1 - 4 * t + 3 * t * t);
+		Vector4f T2 = Vector4f(2 * t - 3 * t * t);
+		Vector4f T3 = Vector4f(t * t);
 
-		Vector4 Result = startPoint * T0;
+		Vector4f Result = startPoint * T0;
 		Result = controlPoint1 * T1 + Result;
 		Result = controlPoint2 * T2 + Result;
 		Result = endPoint * T3 + Result;
