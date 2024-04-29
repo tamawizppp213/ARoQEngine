2b0492f94b87059bb47f7d691c950db4155d6dc7
Toide Yutaro <toide213@gmail.com>
2024-04-30 00:42:48 +0900

    fix ラスタライザの設定をリファクタリング

--- ARoQEngine/GraphicsCore/RHI/DirectX12/PipelineState/Include/DirectX12GPURasterizerState.hpp
+++ ARoQEngine/GraphicsCore/RHI/DirectX12/PipelineState/Include/DirectX12GPURasterizerState.hpp
@@ -26,41 +26,48 @@ namespace rhi::directX12
 	/****************************************************************************
 	*				  			GPURasterizerState
 	*************************************************************************//**
-	*  @class     GPURasterizerState
-	*  @brief     DirectX12 rasterizer state
+	/*  @brief     ピクセルを描画するラスタライザステートにおける設定項目を記述するクラスです. 
 	*****************************************************************************/
 	class GPURasterizerState : public rhi::core::GPURasterizerState
 	{
 	public:
-		/****************************************************************************
-		**                Public Function
-		*****************************************************************************/
+		#pragma region Public Function
+		#pragma endregion 
 
-		/****************************************************************************
-		**                Public Member Variables
-		*****************************************************************************/
-		const D3D12_RASTERIZER_DESC& GetRasterizerState() const noexcept { return _rasterizerState; }
+		#pragma region Public Member Variables
+
+		/*!**********************************************************************
+		*  @brief   DirectX12専用の設定項目を返します
+		*************************************************************************/
+		const D3D12_RASTERIZER_DESC& GetDxRasterizerDesc() const noexcept { return _rasterizerState; }
 		
-		/****************************************************************************
-		**                Constructor and Destructor
-		*****************************************************************************/
+		#pragma endregion 
+		
+		#pragma region Public Constructor and Destructor
+
+		/*! @brief デフォルトコンストラクタ*/
 		GPURasterizerState() = default;
 		
+		/*! @brief デストラクタ*/
 		~GPURasterizerState() = default;
 		
+		/*! @brief ラスタライザの基本設定を用いて作成するコンストラクタ*/
 		explicit GPURasterizerState(
 			const gu::SharedPointer<rhi::core::RHIDevice>& device,
 			const rhi::core::RasterizerProperty& rasterizerProperty);
-	
-	protected:
-		/****************************************************************************
-		**                Protected Function
-		*****************************************************************************/
 
-		/****************************************************************************
-		**                Protected Member Variables
-		*****************************************************************************/
+		#pragma endregion 
+
+	protected:
+		#pragma region Protected Function
+		#pragma endregion 
+
+		#pragma region Protected Member Variables
+
+		/*! @brief DirectX12専用の設定項目 */
 		D3D12_RASTERIZER_DESC _rasterizerState = {};
+
+		#pragma endregion
 	};
 }
 #endif
--- ARoQEngine/GraphicsCore/RHI/DirectX12/PipelineState/Source/DirectX12GPUPipelineState.cpp
+++ ARoQEngine/GraphicsCore/RHI/DirectX12/PipelineState/Source/DirectX12GPUPipelineState.cpp
@@ -46,7 +46,7 @@ void GPUGraphicsPipelineState::CompleteSetting()
 	desc.GS                    = _geometryShaderState ? static_cast<GPUShaderState*>      (_geometryShaderState.Get())->GetShader()            : D3D12_SHADER_BYTECODE();
 	desc.DepthStencilState     = _depthStencilState   ? static_cast<GPUDepthStencilState*>(_depthStencilState  .Get())->GetDepthStencilState() : D3D12_DEPTH_STENCIL_DESC();
 	desc.BlendState            = _blendState          ? static_cast<GPUBlendState*>       (_blendState         .Get())->GetBlendState()        : D3D12_BLEND_DESC();
-	desc.RasterizerState       = _rasterizerState     ? static_cast<GPURasterizerState*>  (_rasterizerState    .Get())->GetRasterizerState()   : D3D12_RASTERIZER_DESC();
+	desc.RasterizerState       = _rasterizerState     ? static_cast<GPURasterizerState*>  (_rasterizerState    .Get())->GetDxRasterizerDesc()  : D3D12_RASTERIZER_DESC();
 	desc.Flags                 = D3D12_PIPELINE_STATE_FLAG_NONE;
 	desc.DSVFormat             = _renderPass->GetDepthAttachment().HasValue() ?
 		                         (DXGI_FORMAT)core::PixelFormatInfo::GetConst(_renderPass->GetDepthAttachment()->Format).PlatformFormat : 
