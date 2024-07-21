1a5e14bf1b92bd2ea8ceeaccf4064e96565c0edb
Toide Yutaro <toide213@gmail.com>
2024-07-21 10:35:48 +0900

    update RenderPass

--- ARoQEngine/GraphicsCore/RHI/DirectX12/Core/Include/DirectX12RenderPass.hpp
+++ ARoQEngine/GraphicsCore/RHI/DirectX12/Core/Include/DirectX12RenderPass.hpp
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////
-///             @file   DirectX12RenderPass.hpp
-///             @brief  Render pass
-///             @author Toide Yutaro
-///             @date   2022_08_02
+///  @file   DirectX12RenderPass.hpp
+///  @brief  レンダーターゲットとデプスステンシルにおけるFrameBufferの書き込み開始時, 終了時の設定項目
+///  @author Toide Yutaro
+///  @date   2024_07_21
 //////////////////////////////////////////////////////////////////////////////////
 #pragma once
 #ifndef DIRECTX12_RENDER_PASS_HPP
@@ -26,44 +26,51 @@ namespace rhi::directX12
 	/****************************************************************************
 	*				  			RHIRenderPass
 	****************************************************************************/
-	/* @class     RHIRenderPass
-	*  @brief     Render pass (not use in directX12)
+	/* @brief     レンダーターゲットとデプスステンシルにおけるFrameBufferの書き込み開始時, 終了時の設定項目
 	*****************************************************************************/
 	class RHIRenderPass : public  rhi::core::RHIRenderPass
 	{
 	public:
-		/****************************************************************************
-		**                Public Function
-		*****************************************************************************/
+		#pragma region Public Function
 
-		/****************************************************************************
-		**                Public Property
-		*****************************************************************************/
-		/* @brief : set debugging name. */
-		void SetName(const gu::tstring& name) { _name = name; };
+		#pragma endregion
 
-		/****************************************************************************
-		**                Constructor and Destructor
-		*****************************************************************************/
+		#pragma region Public Property
+		
+		/*!**********************************************************************
+		*  @brief     複数のレンダーターゲットとデプスステンシルそれぞれのクリアカラーを設定します.
+		*  @param[in] const gu::tstring& デバッグ表示名
+		*  @return    void
+		*************************************************************************/
+		virtual void SetName(const gu::tstring& name) override { _name = name; };
+
+		#pragma endregion
+
+		#pragma region Public Constructor and Destructor
+		/*! @brief デフォルトコンストラクタ*/
 		RHIRenderPass() = default;
 
+		/*! @brief デストラクタ*/
 		~RHIRenderPass() = default;
 
+		/*! @brief 各レンダーターゲットとデプスステンシルの初期化*/
 		explicit RHIRenderPass(const gu::SharedPointer<core::RHIDevice>& device, const gu::DynamicArray<core::Attachment>& colors, const gu::Optional<core::Attachment>& depth = {}) : 
 			core::RHIRenderPass(device, colors, depth){}
 
+		/*! @brief 一つのレンダーターゲットとデプスステンシルを使った初期化*/
 		explicit RHIRenderPass(const gu::SharedPointer<core::RHIDevice>& device, const core::Attachment& color, const gu::Optional<core::Attachment>& depth = {}) :
 			core::RHIRenderPass(device, color, depth){};
 
-	protected:
-		/****************************************************************************
-		**                Protected Function
-		*****************************************************************************/
+		#pragma endregion
 
-		/****************************************************************************
-		**                Protected Property
-		*****************************************************************************/
+	protected:
+		#pragma region Protected Function
+
+		#pragma endregion
+
+		#pragma region Protected Property
 		gu::tstring _name = L"";
+		#pragma endregion
 	};
 }
 #endif
